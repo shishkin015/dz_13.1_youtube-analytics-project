b061cd3e944681c2e826d2816c1e2014993219ec
kirillskyeng <kirill.netreba@skyeng.ru>
2023-05-15 11:08:00 +0300

    Change sample channels

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+API_KEY = os.environ.get('YT_API_KEY')
 
 
 class Channel:
@@ -5,8 +11,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
