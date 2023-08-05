7f2e6a0ddfc1a92e66a962fca6a10fc035d82112
shishkin015 <shishkin015@gmail.com>
2023-08-05 17:30:15 +0300

    dz

--- src/channel.py
+++ src/channel.py
@@ -12,24 +12,70 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        # id канала
+        self.__channel_id = channel_id
         # создать специальный объект для работы с API
         youtube = Channel.get_service()
 
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
+        # название канала
         self.title = channel['items'][0]['snippet']['title']
+        # описание канала
         self.description = channel['items'][0]['snippet']['description']
-        self.views_count = channel['items'][0]['statistics']['viewCount']
-        self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
-        self.subscribers_count = channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel['items'][0]['statistics']['videoCount']
+        # общее количество просмотров
+        self.views_count = int(channel['items'][0]['statistics']['viewCount'])
+        # ссылка на канал
+        self.url = "https://www.youtube.com/channel/" + self.__channel_id
+        # количество подписчиков
+        self.subscribers_count = int(channel['items'][0]['statistics']['subscriberCount'])
+        # количество видео
+        self.video_count = int(channel['items'][0]['statistics']['videoCount'])
+
+    @property
+    def channel_id(self) -> str:
+        """Возвращает id канала."""
+        return self.__channel_id
+
+    def __str__(self) -> str:
+        """Выводит в консоль информацию о канале."""
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        """Возвращает сумму двух каналов."""
+        result = self.subscribers_count + other.subscribers_count
+        return result
+
+    def __sub__(self, other):
+        """Возвращает разность двух каналов."""
+        result = self.subscribers_count - other.subscribers_count
+        return result
+
+    def __gt__(self, other):
+        """Возвращает True, если количество подписчиков канала больше другого."""
+        return self.subscribers_count > other.subscribers_count
+
+    def __lt__(self, other):
+        """Возвращает True, если количество подписчиков канала меньше другого."""
+        return self.subscribers_count < other.subscribers_count
+
+    def __eq__(self, other):
+        """Возвращает True, если количество подписчиков канала равно другому."""
+        return self.subscribers_count == other.subscribers_count
+
+    def __ge__(self, other):
+        """Возвращает True, если количество подписчиков канала больше или равно другому."""
+        return self.subscribers_count >= other.subscribers_count
+
+    def __le__(self, other):
+        """Возвращает True, если количество подписчиков канала меньше или равно другому."""
+        return self.subscribers_count <= other.subscribers_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         youtube = Channel.get_service()
 
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     @classmethod
